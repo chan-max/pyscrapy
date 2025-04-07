57fa9990f270bf52c6a3185ae8c7f8010e8cdd68
chan-max <jackieontheway666@gmail.com>
2025-04-08 06:47:27 +0800

    dev

---
+++ main.py
@@ -0,0 +1,24 @@
+'''
+Author: chan-max jackieontheway666@gmail.com
+Date: 2025-04-08 06:42:23
+LastEditors: chan-max jackieontheway666@gmail.com
+LastEditTime: 2025-04-08 06:46:36
+FilePath: /pyscrapy-1/main.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+import os
+import sys
+import time
+import random
+import requests
+import json
+
+# 一个网络爬图脚本
+
+# 1. 获取图片url
+# 2. 下载图片
+# 3. 保存图片
+# 4. 打印图片信息
+# 5. 打印图片url
+# 6. 打印图片url
+
